03aef50d633b30506cb29e3af82d0a2431aee6eb
jay.mbugua.ph@gmail.com <jammie@DESKTOP-CDCBFL2.>
2024-07-09 13:58:39 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,24 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(name=data['name'], image=data['image'], price=data['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get_or_404(plant_id)
+        return jsonify(plant.to_dict())
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,9 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Column, Integer, String, Float
+
 
 db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    image = Column(String(200))
+    price = Column(Float, nullable=False, default=0.0)
 
-    id = db.Column(db.Integer, primary_key=True)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+        }
